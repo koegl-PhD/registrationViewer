70b73367182fcf29fe8bb10c41b024e758dd791d
koegl <koeglfryderyk@gmail.com>
2025-02-02 18:00:00 +0100

    started adding task and patient loops

--- registrationViewer/registrationViewer.py
+++ registrationViewer/registrationViewer.py
@@ -5,7 +5,7 @@ import logging
 import os
 import time
 
-from typing import Optional, List, Any, Literal
+from typing import Optional, List, Any, Literal, Dict, List, Union
 
 import numpy as np
 
@@ -159,15 +159,28 @@ class registrationViewerWidget(ScriptedLoadableModuleWidget, VTKObservationMixin
         self.path_study_data_master: str = r"/home/koeglf/Documents/code/registrationViewer/registrationViewer/Resources/example_study/data_master.json"
         self.study_data_master: 'study_loading.StudyData' = study_loading.StudyData(
             self.path_study_data_master)
+        self.current_data_dict: Dict[str,
+                                     Dict[str, Union[str, List[str]]]] = {}
+
         self.current_radiologist_id: str = ""
-        self.current_task: 'tasks.Task' = tasks.Task.NONE
+
+        self.current_patient_idx: int = 0
+
+        self.current_task_idx: int = 0
+        self.tasks = [tasks.Task.LYMPH_NODE,
+                      tasks.Task.CAROTIS_GABEL,
+                      tasks.Task.A_VERTEBRALIS,
+                      tasks.Task.RECURRENCE]
 
         self.study_node_points = {
-            tasks.Task.LYMPH_NODE: None,
-            tasks.Task.CAROTIS_GABEL: None,
-            tasks.Task.A_VERTEBRALIS: None,
-            tasks.Task.RECURRENCE: None
+            self.tasks[0]: None,
+            self.tasks[1]: None,
+            self.tasks[2]: None,
+            self.tasks[3]: None
         }
+
+        # task specific
+        self.study_lymphnode_increased = False
 
     def setup(self) -> None:
         """Called when the user opens the module the first time and the widget is initialized."""
@@ -263,12 +276,14 @@ class registrationViewerWidget(ScriptedLoadableModuleWidget, VTKObservationMixin
 
         self.ui_sub_6.start_study_by_user_button.connect("clicked(bool)",
                                                          self.on_user_start_study)
-
-        self.ui_sub_6.study_next_task_button.connect("clicked(bool)",
-                                                     self.on_next_task)
-
         self.ui_sub_6.study_add_point_button.connect("clicked(bool)",
                                                      self.on_study_add_annotation_point)
+        self.ui_sub_6.study_next_task_button.connect("clicked(bool)",
+                                                     self.on_next_task)
+        self.ui_sub_6.study_next_patient_button.connect("clicked(bool)",
+                                                        self.on_study_next_patient)
+        self.ui_sub_6.increasedLymphnodeCheckBox.toggled.connect(
+            self.on_study_lymphnode_increased)
 
         # Buttons
         self.ui_sub_1.simple_ui_button.connect(
@@ -337,9 +352,11 @@ class registrationViewerWidget(ScriptedLoadableModuleWidget, VTKObservationMixin
         slicer.util.resetSliceViews()
         self.ui_sub_4.linearTransformationCheckBox.setEnabled(False)
 
-        # self.dropWidget.load_data_from_dropped_folder(
-        #     "/home/koeglf/data/try_new_preprocessing/SerielleCTs_nii_forHumans/xYbaegYf_mw")
+        self.dropWidget.load_data_from_dropped_folder(
+            "/home/koeglf/data/debugging/SerielleCTs_nii_forHumans/LB9oATPd0mE")
         # utils.temp_load_data(self)
+
+        slicer.util.setDataProbeVisible(False)
 
     def update_current_layout(self, layout: view_logic.Layout) -> None:
         self.current_layout = layout
@@ -613,6 +630,9 @@ class registrationViewerWidget(ScriptedLoadableModuleWidget, VTKObservationMixin
         self.ui_sub_2.radiologistSetCheckBox.setChecked(True)
         self.ui_sub_2.start_study_button.toolTip = f"Press to start the study with {radiologist_name}"  # nopep8
 
+        self.current_patient_list = self.study_data_master.patient_list(self.current_radiologist_id)  # nopep8
+        self.current_patient_idx = 0
+
     def on_start_study(self) -> None:
         self.current_radiologist_id = 'rad_1'
         self.on_simple_ui()
@@ -676,16 +696,40 @@ class registrationViewerWidget(ScriptedLoadableModuleWidget, VTKObservationMixin
         1. when task is started data should be shown
 
         """
-        self.current_task = tasks.Task.LYMPH_NODE
         tasks.show_generic_task_ui(self.ui_sub_6,
                                    self.current_task,
                                    1,
                                    9)
 
-    def on_next_task(self) -> None:
-        pass
+        self.ui_sub_6.study_next_patient_button.setVisible(False)
+        self.ui_sub_6.study_next_task_button.setVisible(True)
 
-    def on_study_add_annotation_point(self,) -> None:
+        self.ui_sub_6.current_case_label.setText(
+            self.current_patient_list[self.current_patient_idx][1])
+
+        self.ui_sub_6.increasedLymphnodeCheckBox.setEnabled(True)
+        self.ui_sub_6.increasedLymphnodeCheckBox.setVisible(True)
+
+    def on_study_next_patient(self) -> None:
+        self.current_patient_idx += 1
+        self.current_task_idx = 0
+
+        self.on_next_task()
+
+    def on_next_task(self) -> None:
+        self.ui_sub_6.increasedLymphnodeCheckBox.setVisible(False)
+        self.current_task_idx += 1
+
+        tasks.show_generic_task_ui(self.ui_sub_6,
+                                   self.current_task,
+                                   1,
+                                   9)
+
+        if self.current_task_idx == 3:
+            self.ui_sub_6.study_next_patient_button.setVisible(True)
+            self.ui_sub_6.study_next_task_button.setVisible(False)
+
+    def on_study_add_annotation_point(self) -> None:
 
         volume_name = self.node_fixed.GetName()
 
@@ -714,7 +758,8 @@ class registrationViewerWidget(ScriptedLoadableModuleWidget, VTKObservationMixin
         utils.show_node_only_in_views(self.study_node_points[self.current_task],
                                       self.views_first_row)
 
-        self.ui_sub_6.study_next_task_button.setVisible(True)
+    def on_study_lymphnode_increased(self) -> None:
+        self.study_lymphnode_increased = not self.study_lymphnode_increased
 
     def on_synchronise_views_wth_trasform(self) -> None:
         if not self._synchronisation_checks():
@@ -1267,6 +1312,10 @@ class registrationViewerWidget(ScriptedLoadableModuleWidget, VTKObservationMixin
     @property
     def node_transform_nonlinear(self) -> Any:
         return self.ui_sub_3.inputSelector_transformation.currentNode()
+
+    @property
+    def current_task(self) -> tasks.Task:
+        return self.tasks[self.current_task_idx]
 
 
 class registrationViewerLogic(ScriptedLoadableModuleLogic):
--- registrationViewer/registrationViewerLib/tasks.py
+++ registrationViewer/registrationViewerLib/tasks.py
@@ -16,12 +16,30 @@ Find level X lymph node in the Fixed image (top row).\n\
 Set a point in its center and select if it changed in size.\n\
 The ROI of the lymph node is marked in the moving image (bottom row).""",
 
-    Task.CAROTIS_GABEL: "",
+    Task.CAROTIS_GABEL: """
+Find the carotid bifurcation in the Fixed image (top row).
+Set a point in the center of the bifurcation.
+The carotid bifurcation is marked in the moving image (bottom row).
+""",
 
-    Task.A_VERTEBRALIS: "",
+    Task.A_VERTEBRALIS: """
+Find the A. vertebralis in the Fixed image (top row).
+Set a point in the center of the A. vertebralis.
+The A. vertebralis is marked in the moving image (bottom row).
+""",
 
-    Task.RECURRENCE: ""
+    Task.RECURRENCE: """
+Decide if a recurrence is present in the Fixed image (top row).
+If yes, set a point in the center of the recurrence.
+TUMOR TEXT
+    """
 }
+
+
+class TransformType(Enum):
+    NONE = "none"
+    LINEAR = "linear"
+    NONLINEAR = "nonlinear"
 
 
 class Case():
@@ -42,7 +60,7 @@ def show_generic_task_ui(
     ui.synchronise_views_general.setVisible(True)
 
     ui.current_case_label.setVisible(True)
-    ui.current_case_label.setText(Case(current_case_num, total_case_count))
+    # ui.current_case_label.setText(Case(current_case_num, total_case_count))
 
     ui.study_current_task_description_label.setText(TASK_DESCRIPTIONS[task])
     ui.study_current_task_description_label.setVisible(True)
