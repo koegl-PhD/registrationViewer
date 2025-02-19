ffe6f89cbcd027334c72c57e0441c276630ec11f
koegl <koeglfryderyk@gmail.com>
2025-02-19 13:26:50 +0100

    ADDED TWO NEW TASKS

--- registrationViewer/registrationViewerLib/tasks.py
+++ registrationViewer/registrationViewerLib/tasks.py
@@ -5,28 +5,41 @@ from typing import Dict, Union
 
 class Task(Enum):
     NONE = "none"
+    CAROTIS_GABEL_L = "carotis_gabel_l"
+    A_VERTEBRALIS_L = "a_vertebralis_l"
+    CAROTIS_GABEL_R = "carotis_gabel_l"
+    A_VERTEBRALIS_R = "a_vertebralis_l"
     LYMPH_NODE = "lymph_node"
-    CAROTIS_GABEL = "carotis_gabel"
-    A_VERTEBRALIS = "a_vertebralis"
     RECURRENCE = "recurrence"
 
 
 TASK_DESCRIPTIONS = {
 
+    Task.CAROTIS_GABEL_L: """
+Find the carotid bifurcation LEFT in the Fixed image (top row).
+Set a point in the center of the bifurcation.
+The carotid bifurcation is marked in the moving image (bottom row).""",
+
+    Task.A_VERTEBRALIS_L: """
+Find the A. vertebralis LEFT in the Fixed image (top row).
+Set a point in the center of the A. vertebralis.
+The A. vertebralis is marked in the moving image (bottom row).""",
+
+
+    Task.CAROTIS_GABEL_R: """
+Find the carotid bifurcation RIGHT in the Fixed image (top row).
+Set a point in the center of the bifurcation.
+The carotid bifurcation is marked in the moving image (bottom row).""",
+
+    Task.A_VERTEBRALIS_R: """
+Find the A. vertebralis RIGHT in the Fixed image (top row).
+Set a point in the center of the A. vertebralis.
+The A. vertebralis is marked in the moving image (bottom row).""",
+
     Task.LYMPH_NODE: """
 Find level X lymph node in the Fixed image (top row).\n\
 Set a point in its center and select if it changed in size.\n\
 The ROI of the lymph node is marked in the moving image (bottom row).""",
-
-    Task.CAROTIS_GABEL: """
-Find the carotid bifurcation in the Fixed image (top row).
-Set a point in the center of the bifurcation.
-The carotid bifurcation is marked in the moving image (bottom row).""",
-
-    Task.A_VERTEBRALIS: """
-Find the A. vertebralis in the Fixed image (top row).
-Set a point in the center of the A. vertebralis.
-The A. vertebralis is marked in the moving image (bottom row).""",
 
     Task.RECURRENCE: """
 Decide if a recurrence is present in the Fixed image (top row).
