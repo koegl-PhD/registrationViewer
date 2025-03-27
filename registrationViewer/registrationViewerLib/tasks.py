eb0f2b5ed480b437c36ceeca89035572353a960b
koegl <koeglfryderyk@gmail.com>
2025-03-27 11:46:18 +0100

    add foirmat placehgolder to lymphnode task descripotno

--- registrationViewer/registrationViewerLib/tasks.py
+++ registrationViewer/registrationViewerLib/tasks.py
@@ -49,7 +49,7 @@ Der Abgang ist im Moving Bild markiert (untere Reihe).
 """,
 
     Task.LYMPH_NODE: """
-Finde den Lymphknoten der Stufe X im Fixed Bild (obere Reihe).
+Finde den Lymphknoten im Fixed Bild (obere Reihe) ({lymphnode_description}).
 Setze einen Punkt in die Mitte des Lymphknotens und wähle, ob er sich in der Größe verändert hat.
 Der Lymphknoten ist im Moving Bild markiert (untere Reihe).
 """,
