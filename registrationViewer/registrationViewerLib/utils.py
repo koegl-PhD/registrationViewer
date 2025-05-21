f168e0e55039d244a9104c836a4c142ebf20c146
koegl <koeglfryderyk@gmail.com>
2025-05-21 16:23:17 +0200

    remove hiding console hardcoded

--- registrationViewer/registrationViewerLib/utils.py
+++ registrationViewer/registrationViewerLib/utils.py
@@ -311,8 +311,7 @@ def set_ui_simplification(self: "registrationViewerWidget") -> None:
     ).self().reloadCollapsibleButton.visible = value
 
     # hide python console
-    # slicer.util.setPythonConsoleVisible(value)
-    slicer.util.setPythonConsoleVisible(True)
+    slicer.util.setPythonConsoleVisible(value)
 
 
 def print_affine_matrix(transformNode):
