0e9dbe41da6df58f64998e1dbad9f99467fd119c
koegl <koeglfryderyk@gmail.com>
2025-05-22 11:53:25 +0200

    added file wit texts for titles, contents and buttons

---
+++ registrationViewer/registrationViewerLib/texts.py
@@ -0,0 +1,63 @@
+from enum import Enum
+
+
+class Titles(Enum):
+    NONE = ""
+
+    USER_ICONS = "STEUERUNGSANLEITUNG"
+    STUDY_DESCRIPTION = "STUDIENBESCHREIBUNG"
+
+    WARNING = "Warnung"
+
+    WARNING_POINT_EXISTS = "Punkt {{insert}} existiert bereits"
+
+    STUDY_INSTRUCTIONS = "STEUERUNGSANLEITUNG"
+
+    STUDY_PAUSED = "STUDIE PAUSIERT"
+
+    STUDY_FINISHED = "STUDIE BEENDET"
+
+
+class Contents(Enum):
+
+    NONE = ""
+    STUDY_DESCRIPTION = """
+
+        Im Rahmen dieser Studie werden Sie mehrere Annotierungsaufgaben bearbeiten, die alle demselben Muster folgen.
+        Sie sehen zwei Reihen von CT-Scans, wobei jedes Paar immer vom selben Patienten stammt.
+        Die obere Reihe zeigt den aktuellsten Scan, den wir „Fixed“ nennen; die untere Reihe zeigt einen älteren Scan, den wir „Moving“ nennen.
+
+        Bei einigen Aufgaben sind die beiden Bilder miteinander registriert. Sie können diese Registrierung mit der Taste „t“ oder über die Schaltfläche „SOMETHING“ ein- und ausschalten. Dadurch wird Ihre Mausbewegung zwischen den beiden Scans gekoppelt, das heißt: Die Position Ihrer Maus in einem Scan wird an der entsprechenden Stelle im anderen Scan angezeigt.
+
+        Achten Sie darauf, die Registrierung zu deaktivieren, wenn Sie von einer Ansicht zur nächsten wechseln.
+        In jeder Aufgabe müssen Sie einen Punkt in der oberen Reihe (dem neueren Scan) setzen, der einer Struktur im unteren Scan entspricht.
+        """
+
+    WARNING_REMOVE_RECURRENCE_POINT = "Möchten Sie den Punkt entfernen, den Sie bereits für das Rezidiv gesetzt habt?"
+
+    QUESTION_OVERWRITE_POINT = "Wollen Sie den Punkt überschreiben?"
+
+    OK_TO_RESMUE = "Klicken Sie auf OK, um fortzufahren"
+
+    STUDY_FINISHED = "Sie haben die Studie abgeschlossen"
+
+
+class Buttons(Enum):
+    NONE = ""
+
+    CENTER_ON_USER_POINT = "Zentriere auf deinen Punkt"
+    CENTER_ON_GROUND_TRUTH_POINT = "Zentriere auf den Referenzpunkt"
+    CENTER_ON_GROUND_TRUTH_LYMPHNODE = "Zentriere auf den Referenzlymphknoten"
+
+    ADD_POINT_BUTTON = "Punkt hinzufügen"
+    NEXT_TASK_BUTTON = "Nächste Aufgabe"
+
+    DROPDOWN_UNCHANGED = "Größe unverändert"
+    DROPDOWN_INCREASED = "Größe vergrößert"
+    DROPDOWN_DECREASED = "Größe verkleinert"
+    DROPDOWN_RECURRENCE_PRESENT = "Rezidiv vorhanden"
+
+    TURN_TRANSFORMATION_ON = "Transformation aktivieren"
+    TURN_TRANSFORMATION_OFF = "Transformation deaktivieren"
+
+    START_STUDY = "Studie starten"
