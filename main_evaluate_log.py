a55e82a8d92d588cb8ea9c27ed60ea56996c08af
koegl <koeglfryderyk@gmail.com>
2025-07-08 16:01:03 +0200

    cehck if recurrecne was detected proeoprly

--- main_evaluate_log.py
+++ main_evaluate_log.py
@@ -5,17 +5,26 @@ import numpy as np
 
 from registrationViewer.registrationViewerLib import log_evaluation, all_evaluations
 
-path = r"/home/fryderyk/Downloads/rad_test.log"
+path = r"/home/koeglf/Downloads/rad_test.log"
 
 df = log_evaluation.load_log_v2_to_df(path)
 
-all_evaluations.plot_duration_by_task_and_transform(
-    df, type='bar', significance=True)
-all_evaluations.plot_duration_by_task_and_transform(
-    df, type='violin', significance=True)
+# all_evaluations.plot_duration_by_task_and_transform(
+#     df, type='bar', significance=True)
+# all_evaluations.plot_duration_by_task_and_transform(
+#     df, type='violin', significance=True)
 # print 5th and 5th last row from the df
 
 stats = all_evaluations.statistical_significance_duration(df)
+
+gt = all_evaluations.get_recurrence_gt()
+
+rt = all_evaluations.get_rad_recurrence(
+    r"/home/koeglf/data/registrationStudy/study_output/rad_ihssan_20250704/")
+
+
+tpr, fpr, tnr, fnr, accuracy, result = all_evaluations.get_recurrence_present_values(
+    gt, rt)
 
 # save stats to .csv
 path = "/home/fryderyk/Documents/code/registrationViewer/stats.csv"
@@ -23,3 +32,15 @@ path = "/home/fryderyk/Documents/code/registrationViewer/stats.csv"
 
 
 x = 0
+
+
+"""
+incorrcet recurrence:
+a9ebcF7RKU4 (linear)
+6vkfAvGWUPg (none)
+I307KZkh1VM (nonlinear)
+JlS0Cl1K0 (nonlinear)
+7sp2FiVa4WI (none)
+2pO8AtRxHAg (none)
+TwU508CCA9Y (linear)
+"""
